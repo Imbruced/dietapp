333d30cdd3def841074fb477b54929c95ea95e59
Bartosz Sroka <48265366+bisroka@users.noreply.github.com>
2020-08-05 22:43:28 +0200

    Complete frontend environment (#17)

---
+++ src/main/resources/templates/build/precache-manifest.d6bf6247bedb2dc1462c9ccf22c0ed51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff3fd4550e2a2abdfdcd720ebe64dfb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e2f7e1212cace9c3501",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a967c059fdec0bfd8aac",
+    "url": "./static/js/2.12a28eef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.12a28eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2f7e1212cace9c3501",
+    "url": "./static/js/main.5d630e8c.chunk.js"
+  },
+  {
+    "revision": "ba3b5bd43745e7647455",
+    "url": "./static/js/runtime-main.7f1d4352.js"
+  }
+]);
