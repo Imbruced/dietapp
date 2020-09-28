0d4f24ba4dbab56ddf1004e8a141ae65be8826a1
Bartosz <bartosz.sroka4@gmail.com>
2020-09-28 15:21:37 +0200

    Refactor complete

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,52 +1,53 @@
 import React, { Component } from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import "./style/style.css";
 
-import Home from "./Templates/Home/Home";
-import Search from "./Templates/Search/Search";
-import Product from "./Templates/Product/Product";
-import Login from "./Templates/Login/Login";
-import Register from "./Templates/Register/Register";
-import Diary from "./Templates/Diary/Diary";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
+import Home from "./Layout/Home";
+import Search from "./Layout/Search";
+import Product from "./Layout/Product";
+import Login from "./Layout/Login";
+import Register from "./Layout/Register";
+import MyDiary from "./Layout/MyDiary";
 
 import Hamburger from "./Components/Hamburger";
 import Menu from "./Components/Menu";
 import Footer from "./Components/Footer";
 
-import { GlobalStyle } from "./App.style";
-import { WrapperStyled } from "./App.style";
-import { DesktopWrapper } from "./App.style";
+const GlobalStyle = createGlobalStyle`
+      body{
+        overflow-x: hidden;
+        width:100vw;
+}
+
+    body, h1,h2,h3,h4,h5,h6{
+        margin:0;
+        padding:0;
+        font-family: "Charter";
+        font-weight: 100;
+        font-style: italic;
+        color: #323232;
+    }
+    *, *::before, *::after{
+        box-sizing: border-box;   
+        margin: 0;
+        padding: 0; 
+    }
+    *:focus {
+    outline: none;
+}
+a{    text-decoration: none;}
+`;
+
+const WrapperStyled = styled.main`
+  display: block;
+  position: relative;
+  min-height: 100vh;
+`;
 
 class App extends Component {
   state = {
-    value: "",
     isMenuVisible: false,
-    data: [],
   };
-  handleChange = (event) => {
-    this.setState({
-      value: event.target.value,
-    });
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.value.length === 0) return;
-    if (prevState.value !== this.state.value) {
-      const API = `http://localhost:5050/products?name=${this.state.value}`;
-
-      fetch(API, {
-        method: "GET",
-        // headers: "application/json",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          this.setState({
-            data: data,
-          });
-        })
-        .catch((err) => console.log(err));
-    }
-  }
 
   menuButtonClickHandler = () => {
     const isMenuVisible = !this.state.isMenuVisible;
@@ -59,54 +60,43 @@ class App extends Component {
       <>
         <GlobalStyle />
         <Router>
-          <DesktopWrapper>
-            <Menu
-              isOpen={this.state.isMenuVisible}
-              click={this.menuButtonClickHandler}
-            />
-            <Hamburger
-              click={this.menuButtonClickHandler}
-              isOpen={this.state.isMenuVisible}
-            />
-            <WrapperStyled>
-              <Switch>
-                <Route path="/" exact>
-                  <Home
-                    click={this.menuButtonClickHandler}
-                    isOpen={this.state.isMenuVisible}
-                  />
-                </Route>
-                <Route path="/search">
-                  <Search
-                    click={this.menuButtonClickHandler}
-                    isOpen={this.state.isMenuVisible}
-                    value={this.state.value}
-                    changeHandler={this.handleChange}
-                    result={this.state.data}
-                  />
-                </Route>
-                <Route path="/product">
-                  <Product
-                    click={this.menuButtonClickHandler}
-                    isOpen={this.state.isMenuVisible}
-                    value={this.state.value}
-                    changeHandler={this.handleChange}
-                    result={this.state.data}
-                  />
-                </Route>
-                <Route path="/login">
-                  <Login />
-                </Route>
-                <Route path="/register">
-                  <Register />
-                </Route>
-                <Route path="/my-diary">
-                  <Diary />
-                </Route>
-              </Switch>
-              {/* <Footer /> */}
-            </WrapperStyled>
-          </DesktopWrapper>
+          <Menu
+            isOpen={this.state.isMenuVisible}
+            click={this.menuButtonClickHandler}
+          />
+          <Hamburger
+            click={this.menuButtonClickHandler}
+            isOpen={this.state.isMenuVisible}
+          />
+          <WrapperStyled>
+            <Switch>
+              <Route path="/" exact>
+                <Home
+                  click={this.menuButtonClickHandler}
+                  isOpen={this.state.isMenuVisible}
+                />
+              </Route>
+              <Route path="/search">
+                <Search
+                  click={this.menuButtonClickHandler}
+                  isOpen={this.state.isMenuVisible}
+                />
+              </Route>
+              <Route path="/product">
+                <Product
+                  click={this.menuButtonClickHandler}
+                  isOpen={this.state.isMenuVisible}
+                />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+            </Switch>
+            <Footer />
+          </WrapperStyled>
         </Router>
       </>
     );
